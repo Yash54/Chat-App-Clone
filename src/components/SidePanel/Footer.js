c45182c055364cb7e898c382e767d85db8028480
Nisarg <nisarg.prajapati21101@gmail.com>
2021-03-22 19:07:34 +0530

    Sidebar

---
+++ src/components/SidePanel/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="copy">
+            Made by Yash, Nisarg, Krishil.
+            <br />
+            © 2021 Chat App. All rights reserved. 
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+import Footer from './Footer';
+
+import UserPanel from "./UserPanel";
+import Channels from "./Channels";
+import DirectMessages from "./DirectMessages";
+import Starred from "./Starred";
+
+class SidePanel extends React.Component {
+  render() {
+    const { currentUser, primaryColor } = this.props;
+
+    return (
+      <Menu
+        size="large"
+        inverted
+        fixed="left"
+        vertical
+        style={{ background: primaryColor, fontSize: "1.2rem" }}
+      >
+        <UserPanel primaryColor={primaryColor} currentUser={currentUser} />
+        <Starred currentUser={currentUser} />
+        <Channels currentUser={currentUser} />
+        <DirectMessages currentUser={currentUser} />
+        <Footer />
+      </Menu>
+    );
+  }
+}
+
+
+export default SidePanel;
